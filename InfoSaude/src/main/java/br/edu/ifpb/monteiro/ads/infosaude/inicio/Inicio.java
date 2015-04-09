25042255dc25cefa3d5bcc84a5739c49789d5b1e
Vanderlan Gomes <vanderlan.gs@gmail.com>
2015-04-09 17:22:37 -0300

    Resolvendo problema de compatibilidade com Java 8.

--- InfoSaude/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/inicio/Inicio.java
+++ InfoSaude/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/inicio/Inicio.java
@@ -3,14 +3,11 @@ package br.edu.ifpb.monteiro.ads.infosaude.inicio;
 
 import br.edu.ifpb.monteiro.ads.infosaude.Dao.PessoaDao;
 import br.edu.ifpb.monteiro.ads.infosaude.modelo.Pessoa;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 
 public class Inicio {
+    
   
     
     
@@ -20,7 +17,7 @@ public class Inicio {
         Pessoa p = new Pessoa();
         p.setIdPessoa(Long.MIN_VALUE);
         p.setNome("fgdfggdgdgd");
-        p.setDataNascimento(Date.from(Instant.EPOCH));
+        p.setDataNascimento(new Date(1991, 04, 10));
         p.setCartaoSUS("1231312466778534");
         p.setCpf("42345677545");
         dao.salvar(p);
