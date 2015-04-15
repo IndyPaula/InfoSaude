fa80f66ae47d470cd1ab0d1e01aaa952fc043a33
JeffersonEmanuel <jefferson.ecs@gmail.com>
2015-04-15 18:19:25 -0300

    Adicionando a Classe AtendimentoDomiciliar ao pacote modelo

---
+++ Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/modelo/AtendimentoDomiciliar.java
@@ -0,0 +1,53 @@
+package br.edu.ifpb.monteiro.ads.infosaude.modelo;
+
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.interfaces.Identificavel;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Jefferson Emanuel Caldeira da Silva <jefferson.ecs@gmail.com>
+ * @date 15/04/2015
+ */
+@Entity(name = "atendimento_domiciliar")
+@SequenceGenerator(name = "atendimento_domiciliar_seq", sequenceName = "atendimento_domiciliar_seq", initialValue = 1, allocationSize = 1)
+public class AtendimentoDomiciliar implements Identificavel<AtendimentoDomiciliar> {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "atendimento_domiciliar_seq")
+    private Long id;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_atendimento", nullable = false)
+    private Date dataAtendimento;
+    
+    
+    @OneToOne
+    private Paciente paciente;
+    
+    
+    @Column(name = "motivo_visita", length = 45)
+    private String motivoVisita;
+
+    
+    
+    
+    
+    @Override
+    public Long getId() {
+        return id;
+    }
+    
+    
+    
+    
+    
+}
