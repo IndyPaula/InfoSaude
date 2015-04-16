bad7ba8f1a0c99ac63acd2f4ef005a94235bea78
Vanderlan Gomes <vanderlan.gs@gmail.com>
2015-04-16 17:13:34 -0300

    Controlando tempo da sessão de usuário e aperfeiçoando testes de unidade.

--- Sistema/003-Implementacao/src/test/java/br/edu/ifpb/monteiro/ads/infosaude/dao/LoginAdminDaoIT.java
+++ Sistema/003-Implementacao/src/test/java/br/edu/ifpb/monteiro/ads/infosaude/dao/LoginAdminDaoIT.java
@@ -11,24 +11,25 @@ import static org.junit.Assert.*;
 /**
  *
  * @author Vanderlan Gomes<vanderlan.gs@gmail.com>
+ * Criado em 16 de Abril de 2015
  */
 public class LoginAdminDaoIT {
-    
+
     public LoginAdminDaoIT() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -39,8 +40,6 @@ public class LoginAdminDaoIT {
     @Test
     public void senhaNull() {
         System.out.println("Teste de Senha Null");
-    
-        System.out.println("Teste de usuários inexistente");
 
         LoginAdminDao instance = new LoginAdminDao();
         LoginAdmin expResult = null;
@@ -48,12 +47,10 @@ public class LoginAdminDaoIT {
 
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void senhaIncorreta() {
         System.out.println("Teste de Senha incorreta");
-    
-      System.out.println("Teste de usuários inexistente");
 
         LoginAdminDao instance = new LoginAdminDao();
         LoginAdmin expResult = null;
@@ -61,7 +58,7 @@ public class LoginAdminDaoIT {
 
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void usuarioInexistente() {
         System.out.println("Teste de usuários inexistente");
@@ -72,7 +69,7 @@ public class LoginAdminDaoIT {
 
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void usuarioValido() {
         System.out.println("Teste de usuário válido");
@@ -80,7 +77,7 @@ public class LoginAdminDaoIT {
         LoginAdminDao instance = new LoginAdminDao();
         LoginAdmin expResult = new LoginAdmin();
         expResult.setNome("Vanderlan");
-        
+
         LoginAdmin result = instance.efetuarLogin("Vanderlan", "123");
 
         assertEquals(expResult.getNome(), result.getNome());
