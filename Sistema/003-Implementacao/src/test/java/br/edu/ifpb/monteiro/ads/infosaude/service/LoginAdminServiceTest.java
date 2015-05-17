8fd9b94ee3ba9f5181e1f119e92cc313b37e1175
Vanderlan <vanderlan.gs@gmail.com>
2015-05-17 01:09:11 -0300

    Adicionando suporte ao Mockito e finalizando testes na camada de Service do Login.

--- Sistema/003-Implementacao/src/test/java/br/edu/ifpb/monteiro/ads/infosaude/service/LoginAdminServiceTest.java
+++ Sistema/003-Implementacao/src/test/java/br/edu/ifpb/monteiro/ads/infosaude/service/LoginAdminServiceTest.java
@@ -1,10 +1,15 @@
 package br.edu.ifpb.monteiro.ads.infosaude.service;
 
+import br.edu.ifpb.monteiro.ads.infosaude.dao.interfaces.LoginAdminDaoIF;
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.LoginAdmin;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 
 /**
  *
@@ -13,20 +18,31 @@ import org.junit.Test;
 public class LoginAdminServiceTest {
 
     private static LoginAdminService service;
+
+    private LoginAdmin adm;
     
+    @Mock
+    private LoginAdminDaoIF dao;
+
     public LoginAdminServiceTest() {
     }
 
     @BeforeClass
     public static void setUpClass() {
-    
+
         service = new LoginAdminService();
-        
+
     }
-    
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        
+        adm = new LoginAdmin();
+        adm.setLogin("User");
+        adm.setSenhaAdm("123456");
+        
+        when(dao.efetuarLogin("User", "123456")).thenReturn(adm);
     }
 
     @After
@@ -41,10 +57,26 @@ public class LoginAdminServiceTest {
 
         assertEquals(null, service.efetuarLogin(null, null));
     }
-    
+
     @Test
     public void loginSenhaValidos() {
 
-        assertEquals(null, service.efetuarLogin("Vanderlan", "123"));
+        adm = new LoginAdmin();
+        adm.setLogin("User");
+        adm.setSenhaAdm("123456");
+        
+        when(dao.efetuarLogin("User", "123456")).thenReturn(adm);
+
+        LoginAdmin admResult = dao.efetuarLogin("User", "123456");
+        
+        assertEquals(adm.getLogin(), admResult.getLogin());
+    }
+    
+    @Test
+    public void senhaInvalida() {
+
+        LoginAdmin admResult = dao.efetuarLogin("User", "123");
+        
+        assertEquals(null, admResult);
     }
 }
