640aa842de7fa7d272ae3e0745722d8a235b31f1
JeffersonEmanuel <jefferson.ecs@gmail.com>
2015-05-20 18:22:59 -0300

    Cadastro vacinador

---
+++ Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/beans/VacinadorBean.java
@@ -0,0 +1,91 @@
+package br.edu.ifpb.monteiro.ads.infosaude.beans;
+
+import br.edu.ifpb.monteiro.ads.infosaude.dao.excecoes.DaoExcecoes;
+import br.edu.ifpb.monteiro.ads.infosaude.dao.util.JsfUtil;
+import br.edu.ifpb.monteiro.ads.infosaude.enumerations.EnumEstados;
+import br.edu.ifpb.monteiro.ads.infosaude.enumerations.EnumEtnias;
+import br.edu.ifpb.monteiro.ads.infosaude.enumerations.EnumGeneros;
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.Vacinador;
+import br.edu.ifpb.monteiro.ads.infosaude.service.excecoes.ServiceExcecoes;
+import br.edu.ifpb.monteiro.ads.infosaude.service.interfaces.VacinadorServiceIF;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Jefferson Emanuel Caldeira da Silva <jefferson.ecs@gmail.com>
+ * @date 20/05/2015
+ */
+@Model
+public class VacinadorBean {
+
+    private String confirmarSenha;
+
+    @Inject
+    private Vacinador vacinador;
+
+    @Inject
+    private VacinadorServiceIF serviceIF;
+
+    public Date getDataAtual() {
+        Calendar calendar
+                = new Calendar.Builder()
+                .setTimeZone(TimeZone.getTimeZone("America/Sao_Paulo"))
+                .setLocale(new Locale("pt", "br"))
+                .setInstant(new Date())
+                .build();
+        Instant instantAPartirDoCalendar = calendar.toInstant();
+        Date hoje = Date.from(instantAPartirDoCalendar);
+        return hoje;
+    }
+
+    public String salvar() {
+        try {
+
+            serviceIF.salvar(vacinador);
+            JsfUtil.addSuccessMessage("Usuário da UBS cadastrado com sucesso");
+
+        } catch (ServiceExcecoes ex) {
+
+            JsfUtil.addErrorMessage(ex.getMessage());
+
+            return null;
+
+        }
+        return "";
+    }
+
+    public Vacinador getVacinador() {
+        return vacinador;
+    }
+
+    public void setVacinador(Vacinador vacinador) {
+        this.vacinador = vacinador;
+    }
+
+    public EnumEstados[] getEstado() {
+        return EnumEstados.values();
+    }
+
+    public EnumGeneros[] getSexo() {
+        return EnumGeneros.values();
+    }
+
+    public EnumEtnias[] getEtnias() {
+        return EnumEtnias.values();
+    }
+
+    public String getConfirmarSenha() {
+        return confirmarSenha;
+    }
+
+    public void setConfirmarSenha(String confirmarSenha) {
+        this.confirmarSenha = confirmarSenha;
+    }
+
+}
