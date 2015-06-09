7111db7405930f406161cc729bbe45471e2f9eb9
JeffersonEmanuel <jefferson.ecs@gmail.com>
2015-06-09 14:49:52 -0300

    Atualização de vacina ok

--- Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/beans/VacinaBean.java
+++ Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/beans/VacinaBean.java
@@ -1,13 +1,17 @@
 package br.edu.ifpb.monteiro.ads.infosaude.beans;
 
+import br.edu.ifpb.monteiro.ads.infosaude.beans.excecaoes.BeanExcecao;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.util.JsfUtil;
 import br.edu.ifpb.monteiro.ads.infosaude.enumerations.EnumViaAdministracao;
 import br.edu.ifpb.monteiro.ads.infosaude.modelo.Vacina;
 import br.edu.ifpb.monteiro.ads.infosaude.service.excecoes.ServiceExcecoes;
 import br.edu.ifpb.monteiro.ads.infosaude.service.interfaces.VacinaServiceIF;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
@@ -25,7 +29,20 @@ public class VacinaBean {
     @Inject
     private VacinaServiceIF vacinaService;
 
+    private String mensangem;
+
+    private List<Vacina> vacinasFilter;
+
+    private Long idAuxiliar;
+
     public VacinaBean() {
+    }
+
+    @PostConstruct
+    public void init() {
+
+        vacinasFilter = new ArrayList<>();
+
     }
 
     public Date getDataAtual() {
@@ -48,6 +65,65 @@ public class VacinaBean {
         return "buscar_vacina.xhtml";
     }
 
+    public String remover(Vacina vac) {
+
+        if (vac != null) {
+
+            try {
+                vacinaService.remover(vac);
+
+                Thread.sleep(1000);
+
+                JsfUtil.redirect("/InfoSaude/resources/paginas/vacina/buscar_vacina.xhtml?faces-redirect=true");
+                return "buscar_vacina.xhtml";
+
+            } catch (ServiceExcecoes ex) {
+                Logger.getLogger(VacinaBean.class.getName()).log(Level.SEVERE, null, ex);
+                JsfUtil.addErrorMessage("Erro ao tentar remover a vacina");
+            } catch (InterruptedException ex) {
+                Logger.getLogger(VacinaBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+
+            JsfUtil.addErrorMessage("Erro ao tentar remover a Vacina");
+        }
+        return null;
+    }
+
+    public String update() throws BeanExcecao {
+
+        try {
+            vacina.setId(idAuxiliar);
+            vacinaService.atualizar(vacina);
+            JsfUtil.addSuccessMessage("Informações atualizadas com sucesso");
+            return "editar_vacina.xhtml?id=" + vacina.getId();
+
+        } catch (ServiceExcecoes ex) {
+
+            JsfUtil.addErrorMessage(ex.getMessage());
+            Logger.getLogger(VacinaBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public void preparaEdicao() {
+
+        try {
+            vacina = vacinaService.consultarPorId(idAuxiliar);
+        } catch (ServiceExcecoes ex) {
+            Logger.getLogger(VacinaBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public Long getIdAuxiliar() {
+        return idAuxiliar;
+    }
+
+    public void setIdAuxiliar(Long idAuxiliar) {
+        this.idAuxiliar = idAuxiliar;
+    }
+
     public Vacina getVacina() {
         return vacina;
     }
@@ -56,8 +132,33 @@ public class VacinaBean {
         this.vacina = vacina;
     }
 
+    public List<Vacina> getVacinas() {
+        try {
+            return vacinaService.buscarTudo();
+        } catch (ServiceExcecoes ex) {
+            Logger.getLogger(VacinaBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
     public EnumViaAdministracao[] getViaAdministracao() {
         return EnumViaAdministracao.values();
+    }
+
+    public String getMensangem() {
+        return mensangem;
+    }
+
+    public void setMensangem(String mensangem) {
+        this.mensangem = mensangem;
+    }
+
+    public List<Vacina> getVacinasFilter() {
+        return vacinasFilter;
+    }
+
+    public void setVacinasFilter(List<Vacina> vacinasFilter) {
+        this.vacinasFilter = vacinasFilter;
     }
 
 }
