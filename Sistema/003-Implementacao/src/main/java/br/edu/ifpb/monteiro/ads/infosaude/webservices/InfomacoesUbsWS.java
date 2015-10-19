b3b1fe77cd9255db070c75d105f4ae8fde0faa24
Vanderlan <vanderlan.gs@gmail.com>
2015-10-18 22:05:14 -0300

    Disponibilizando lista de ACS e Vacinadores via WS.

--- Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/webservices/InfomacoesUbsWS.java
+++ Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/webservices/InfomacoesUbsWS.java
@@ -1,9 +1,14 @@
 package br.edu.ifpb.monteiro.ads.infosaude.webservices;
 
+import br.edu.ifpb.monteiro.ads.infosaude.dao.ACSDao;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.UnidadeSaudeDao;
+import br.edu.ifpb.monteiro.ads.infosaude.dao.VacinadorDao;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.excecoes.DaoExcecoes;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.util.EntityManagerProducer;
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.ACS;
 import br.edu.ifpb.monteiro.ads.infosaude.modelo.UnidadeSaude;
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.Vacinador;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jws.WebService;
@@ -21,6 +26,8 @@ public class InfomacoesUbsWS {
     private EntityManagerProducer emp;
     private EntityManager em;
     private UnidadeSaudeDao daoUbs;
+    private VacinadorDao daoVacinador;
+    private ACSDao daoAcs;
 
     public InfomacoesUbsWS() {
 
@@ -28,10 +35,14 @@ public class InfomacoesUbsWS {
 
     private void preparaDao() {
         daoUbs = new UnidadeSaudeDao();
+        daoVacinador = new VacinadorDao();
+        daoAcs = new ACSDao();
 
         emp = new EntityManagerProducer();
         em = emp.create();
         daoUbs.setEntityManager(em);
+        daoVacinador.setEntityManager(em);
+        daoAcs.setEntityManager(em);
     }
 
     @WebMethod(operationName = "hello")
@@ -50,6 +61,36 @@ public class InfomacoesUbsWS {
         try {
             preparaDao();
             return daoUbs.obterInstancia();
+        } catch (DaoExcecoes ex) {
+            Logger.getLogger(InfomacoesUbsWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    /**
+     * Retorna as os vacinadores da UBS
+     *
+     * @return
+     */
+    @WebMethod(operationName = "vacinadoresUBS")
+    public List<Vacinador> vacinadoresUBS() {
+        try {
+            preparaDao();
+            return daoVacinador.buscarTudo();
+        } catch (DaoExcecoes ex) {
+            Logger.getLogger(InfomacoesUbsWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    /**
+     * Retorna os agentes comunitários de Saúde da UBS
+     *
+     * @return
+     */
+    @WebMethod(operationName = "acsUBS")
+    public List<ACS> acsUBS() {
+        try {
+            preparaDao();
+            return daoAcs.buscarTudo();
         } catch (DaoExcecoes ex) {
             Logger.getLogger(InfomacoesUbsWS.class.getName()).log(Level.SEVERE, null, ex);
         }
