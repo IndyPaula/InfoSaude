7be07b695deb8faffd954e3e2b4152038d643461
JeffersonEmanuel <jefferson.ecs@gmail.com>
2015-10-19 17:43:32 -0300

    Relatoio imunobiologico correção de commit

--- Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/webservices/InfomacoesUbsWS.java
+++ Sistema/003-Implementacao/src/main/java/br/edu/ifpb/monteiro/ads/infosaude/webservices/InfomacoesUbsWS.java
@@ -1,9 +1,14 @@
 package br.edu.ifpb.monteiro.ads.infosaude.webservices;
 
+import br.edu.ifpb.monteiro.ads.infosaude.dao.ACSDao;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.UnidadeSaudeDao;
+import br.edu.ifpb.monteiro.ads.infosaude.dao.VacinadorDao;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.excecoes.DaoExcecoes;
 import br.edu.ifpb.monteiro.ads.infosaude.dao.util.EntityManagerProducer;
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.ACS;
 import br.edu.ifpb.monteiro.ads.infosaude.modelo.UnidadeSaude;
+import br.edu.ifpb.monteiro.ads.infosaude.modelo.Vacinador;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jws.WebService;
@@ -18,20 +23,26 @@ import javax.persistence.EntityManager;
 @WebService(serviceName = "InfomacoesUbsWS")
 public class InfomacoesUbsWS {
 
-
-    private final EntityManagerProducer emp;
-    private final EntityManager em;
-    private final UnidadeSaudeDao daoUbs;
+    private EntityManagerProducer emp;
+    private EntityManager em;
+    private UnidadeSaudeDao daoUbs;
+    private VacinadorDao daoVacinador;
+    private ACSDao daoAcs;
 
     public InfomacoesUbsWS() {
 
+    }
+
+    private void preparaDao() {
         daoUbs = new UnidadeSaudeDao();
-        
+        daoVacinador = new VacinadorDao();
+        daoAcs = new ACSDao();
+
         emp = new EntityManagerProducer();
         em = emp.create();
         daoUbs.setEntityManager(em);
-
-
+        daoVacinador.setEntityManager(em);
+        daoAcs.setEntityManager(em);
     }
 
     @WebMethod(operationName = "hello")
@@ -48,7 +59,38 @@ public class InfomacoesUbsWS {
     @WebMethod(operationName = "dadosUBS")
     public UnidadeSaude dadosUBS() {
         try {
-            return daoUbs.buscarTudo().get(0);
+            preparaDao();
+            return daoUbs.obterInstancia();
+        } catch (DaoExcecoes ex) {
+            Logger.getLogger(InfomacoesUbsWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    /**
+     * Retorna as os vacinadores da UBS
+     *
+     * @return
+     */
+    @WebMethod(operationName = "vacinadoresUBS")
+    public List<Vacinador> vacinadoresUBS() {
+        try {
+            preparaDao();
+            return daoVacinador.buscarTudo();
+        } catch (DaoExcecoes ex) {
+            Logger.getLogger(InfomacoesUbsWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    /**
+     * Retorna os agentes comunitários de Saúde da UBS
+     *
+     * @return
+     */
+    @WebMethod(operationName = "acsUBS")
+    public List<ACS> acsUBS() {
+        try {
+            preparaDao();
+            return daoAcs.buscarTudo();
         } catch (DaoExcecoes ex) {
             Logger.getLogger(InfomacoesUbsWS.class.getName()).log(Level.SEVERE, null, ex);
         }
